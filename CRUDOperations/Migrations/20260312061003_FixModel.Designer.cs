a9e81d1f417ca449c24d499529e8a218b2ef9032
VenishPatel <venishdpatel2602@gmail.com>
2026-03-12 11:41:38 +0530

    add fileds in database

--- CRUDOperations/Migrations/20260312061003_FixModel.Designer.cs
+++ CRUDOperations/Migrations/20260312061003_FixModel.Designer.cs
@@ -4,6 +4,7 @@ using CRUDOperations.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CRUDOperations.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260312061003_FixModel")]
+    partial class FixModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CRUDOperations/Migrations/20260312061003_FixModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUDOperations.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DiscountOff",
+                table: "ProductDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "OrginalPrice",
+                table: "ProductDetails",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DiscountOff",
+                table: "ProductDetails");
+
+            migrationBuilder.DropColumn(
+                name: "OrginalPrice",
+                table: "ProductDetails");
+        }
+    }
+}
